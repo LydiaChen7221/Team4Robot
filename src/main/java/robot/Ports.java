e45a8eed1d28cea90a63c7f673f4dfa435fd0c22
Summerfrowt <summerfrowt@gmail.com>
2025-03-13 16:52:04 -0400

    Update Ports.java

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -7,10 +7,5 @@ public final class Ports {
     public static final int DRIVER = 1;
   }
 
-   public static final class Drive {
-    public static final int RIGHT_LEADER = 2;
-    public static final int RIGHT_FOLLOWER = 3;
-    public static final int LEFT_LEADER = -2;
-    public static final int LEFT_FOLLOWER = -3;
-  }
+   
 }
